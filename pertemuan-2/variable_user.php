492e6a3f5c95d95b2c4c4998dbb79b162a0d169f
revani18 <revani1811@gmail.com>
2025-03-01 11:27:34 +0700

    menambahkan praktikum pertemuan 2

---
+++ pertemuan-2/variable_user.php
@@ -0,0 +1,10 @@
+<?php
+    // mendefinisikan variables
+    $nama = "Revani";
+    $umur = 21;
+    $bb = 40;
+
+    echo "Nama saya adalah : " .$nama;
+    echo "</br>Umur : " .$umur. "Tahun";
+    echo "</br>Berat Badan : " .$bb. "Kg";
+?>
