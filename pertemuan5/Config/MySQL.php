7bdbe98d7a2124d40a3ff281aac643f2844a7ca0
revani18 <revani1811@gmail.com>
2025-03-21 16:22:30 +0700

    upload praktikum pertemuan5

---
+++ pertemuan5/Config/MySQL.php
@@ -0,0 +1,19 @@
+<?php
+class ConnectDB{
+    public $conn;
+    public function __construct(){
+        $this->conn = mysqli_connect("localhost", "root", "", "bukutamu");
+        if (!$this->conn) {
+            die("Gagal Terkoneksi: " . mysqli_connect_error());
+        }
+
+    }
+    public function db(){
+        return $this->conn;
+    }
+    
+}
+
+$db = new ConnectDB();
+$conn = $db->db();
+?>
